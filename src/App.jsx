fcd16bf53c304587cec29c672a9d029d3d3cd5fb
RudyWu <Rudy.Wu@outlook.com>
2026-01-20 18:14:53 +0800

    複製 week3 檔案

---
+++ src/App.jsx
@@ -0,0 +1,404 @@
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import * as bootstrap from "bootstrap";
+import "./assets/style.css";
+import ProductModal from "./components/ProductModal";
+
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
+
+const INITIAL_TEMPLATE_DATA = {
+  id: "",
+  title: "",
+  category: "",
+  origin_price: "",
+  price: "",
+  unit: "",
+  description: "",
+  content: "",
+  is_enabled: false,
+  imageUrl: "",
+  imagesUrl: [],
+};
+
+function App() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  // const [isAuth, setIsAuth] = useState(false)
+  const [isAuth, setIsAuth] = useState(() => {
+    const token = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("hexToken="))
+      ?.split("=")[1];
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = token;
+      return true;
+    }
+    return false;
+  });
+
+  const [products, setProducts] = useState([]);
+  // 下列為 week2 細圖需要有的 temp code
+  // const [tempProduct, setTempProduct] = useState(null);
+
+  const [templateProduct, setTemplateProduct] = useState(INITIAL_TEMPLATE_DATA);
+  const [modalType, setModalType] = useState();
+
+  const productModalRef = useRef(null);
+
+  const getProducts = async () => {
+    try {
+      const res = await axios.get(
+        `${API_BASE}/api/${API_PATH}/admin/products/all`,
+      );
+      setProducts(Object.values(res.data.products));
+      // console.log(Object.values(res.data.products));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleModalInputChange = (e) => {
+    const { name, value, checked, type } = e.target;
+    setTemplateProduct((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  const handleModalImageChange = (index, value) => {
+    setTemplateProduct((pre) => {
+      const newImage = [...pre.imagesUrl];
+      newImage[index] = value;
+
+      if (
+        value !== "" &&
+        index === newImage.length - 1 &&
+        newImage.length < 5
+      ) {
+        newImage.push("");
+      }
+      if (
+        value === "" &&
+        newImage.length > 1 &&
+        newImage[newImage.length - 1] === ""
+      ) {
+        newImage.pop();
+      }
+
+      return {
+        ...pre,
+        imagesUrl: newImage,
+      };
+    });
+  };
+
+  const handleAddImage = () => {
+    setTemplateProduct((pre) => {
+      const newImage = [...pre.imagesUrl];
+      newImage.push("");
+      return {
+        ...pre,
+        imagesUrl: newImage,
+      };
+    });
+  };
+
+  const handleRemoveImage = () => {
+    setTemplateProduct((pre) => {
+      const newImage = [...pre.imagesUrl];
+      newImage.pop();
+      return {
+        ...pre,
+        imagesUrl: newImage,
+      };
+    });
+  };
+
+  const updateProduct = async (id) => {
+    let url = `${API_BASE}/api/${API_PATH}/admin/product`;
+    let method = "post";
+
+    if (modalType === "edit") {
+      url = `${API_BASE}/api/${API_PATH}/admin/product/${id}`;
+      method = "put";
+    }
+
+    const productData = {
+      data: {
+        ...templateProduct,
+        origin_price: Number(templateProduct.origin_price),
+        price: Number(templateProduct.price),
+        is_enabled: templateProduct.is_enabled ? 1 : 0,
+        imagesUrl: [...templateProduct.imagesUrl.filter((url) => url !== "")],
+      },
+    };
+    try {
+      const res = await axios[method](url, productData);
+      console.log(res.data);
+      getProducts();
+      closeModal();
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  const delProduct = async (id) => {
+    try {
+      const res = await axios.delete(
+        `${API_BASE}/api/${API_PATH}/admin/product/${id}`,
+      );
+      console.log(res.data);
+      getProducts();
+      closeModal();
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const res = await axios.post(`${API_BASE}/admin/signin`, formData);
+      const { token, expired } = res.data;
+      document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+      axios.defaults.headers.common["Authorization"] = token;
+      getProducts();
+      setIsAuth(true);
+    } catch (e) {
+      setIsAuth(false);
+      console.log(e.message);
+      alert("登入失敗，請檢查帳號密碼");
+    }
+  };
+
+  useEffect(() => {
+    // 定義一個只在這個 effect 裡使用的抓資料函式
+    // const getProducts = async () => {
+    //   try {
+    //     const res = await axios.get(`${API_BASE}/api/${API_PATH}/products/all`);
+    //     setProducts(res.data.products);
+    //   } catch (e) {
+    //     console.error(e);
+    //   }
+    // };
+    // 只有當 isAuth 為 true 時，才執行抓資料
+    const token = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("hexToken="))
+      ?.split("=")[1];
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = token;
+    }
+    productModalRef.current = new bootstrap.Modal("#productModal", {
+      keyboard: false,
+    });
+
+    const checkLogin = async () => {
+      try {
+        const res = await axios.post(`${API_BASE}/api/user/check`);
+        console.log("token 驗證結果:", res.data);
+        setIsAuth(true);
+        getProducts();
+      } catch (error) {
+        console.error("token 驗證失敗", error.response);
+      }
+    };
+    checkLogin();
+  }, []);
+
+  const openModal = (type, product) => {
+    // console.log(product);
+    setModalType(type);
+    setTemplateProduct((pre) => ({ ...pre, ...product }));
+    productModalRef.current.show();
+  };
+  const closeModal = () => {
+    productModalRef.current.hide();
+  };
+
+  return (
+    <>
+      {!isAuth ? (
+        <div className="container login">
+          <div className="row justify-content-center">
+            <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
+            <div className="col-8">
+              <form id="form" className="form-signin" onSubmit={handleSubmit}>
+                <div className="form-floating mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="username"
+                    placeholder="name@product.com"
+                    value={formData.username}
+                    onChange={handleInputChange}
+                    required
+                    autoFocus
+                  />
+                  <label htmlFor="username">Email address</label>
+                </div>
+                <div className="form-floating">
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    required
+                  />
+                  <label htmlFor="password">Password</label>
+                </div>
+                <button
+                  className="btn btn-lg btn-primary w-100 mt-3"
+                  type="submit"
+                >
+                  登入
+                </button>
+              </form>
+            </div>
+          </div>
+          <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
+        </div>
+      ) : (
+        <div className="container">
+          <h2>產品列表</h2>
+          <div className="text-end mt-4">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => openModal("create", INITIAL_TEMPLATE_DATA)}
+            >
+              建立新的產品
+            </button>
+          </div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>品牌</th>
+                <th>產品名稱</th>
+                <th>原價</th>
+                <th>售價</th>
+                <th>是否啟用</th>
+                <th>編輯</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products && products.length > 0 ? (
+                products.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.category}</td>
+                    <td scope="row">{item.title}</td>
+                    <td>{item.origin_price}</td>
+                    <td>{item.price}</td>
+                    <td className={`${item.is_enabled && "text-success"}`}>
+                      {item.is_enabled ? "啟用" : "未啟用"}
+                    </td>
+                    <td>
+                      <div
+                        className="btn-group"
+                        role="group"
+                        aria-label="Basic product"
+                      >
+                        <button
+                          type="button"
+                          className="btn btn-outline-primary btn-sm"
+                          onClick={() => openModal("edit", item)}
+                        >
+                          編輯
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-outline-danger btn-sm"
+                          onClick={() => openModal("delete", item)}
+                        >
+                          刪除
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="5">尚無產品資料</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+
+          {/* ==============以下為week2 商品細圖的 code============================== */}
+          {/* <div className="col-md-6"></div>
+            <div className="col-md-6">
+              <h2>單一產品細節</h2>
+              {tempProduct ? (
+                <div className="card mb-3">
+                  <img
+                    src={tempProduct.imageUrl}
+                    className="card-img-top primary-image"
+                    alt="主圖"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {tempProduct.title}
+                      <span className="badge bg-primary ms-2">
+                        {tempProduct.category}
+                      </span>
+                    </h5>
+                    <p className="card-text">
+                      商品描述：{tempProduct.description}
+                    </p>
+                    <p className="card-text">商品內容：{tempProduct.content}</p>
+                    <div className="d-flex">
+                      <p className="card-text text-secondary">
+                        <del>{tempProduct.origin_price}</del>
+                      </p>
+                      元 / {tempProduct.price} 元
+                    </div>
+                    <h5 className="mt-3">更多圖片：</h5>
+                    <div className="d-flex flex-wrap gap-2 justify-content-center">
+                      {tempProduct.imagesUrl.map((url, index) => (
+                        <img
+                          key={index}
+                          src={url}
+                          className="images"
+                          alt="副圖"
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <p className="text-secondary">請選擇一個商品查看</p>
+              )}
+            </div> */}
+          {/* ==============以上為week2 商品細圖的 code============================== */}
+        </div>
+      )}
+
+      <ProductModal
+        modalType={modalType}
+        templateProduct={templateProduct}
+        handleModalInputChange={handleModalInputChange}
+        handleModalImageChange={handleModalImageChange}
+        handleAddImage={handleAddImage}
+        handleRemoveImage={handleRemoveImage}
+        updateProduct={updateProduct}
+        delProduct={delProduct}
+        closeModal={closeModal}
+      />
+    </>
+  );
+}
+
+export default App;
